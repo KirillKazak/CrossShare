301abdfdbff3cda7246698262ab5ec5afb936af8
VasylLelekaAndersen <v.leleka@andersenlab.com>
2022-03-29 12:50:32 +0300

    add converter factory

--- app/src/main/java/com/cross/share/com/di/NetworkModule.kt
+++ app/src/main/java/com/cross/share/com/di/NetworkModule.kt
@@ -4,20 +4,31 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.create
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 class NetworkModule {
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit.Builder {
-        return Retrofit.Builder().baseUrl("https://earthquake.usgs.gov/fdsnws/event/1/").client(okHttpClient)
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit.Builder {
+        return Retrofit.Builder().baseUrl("https://earthquake.usgs.gov/fdsnws/event/1/")
+            .addConverterFactory(converterFactory).client(okHttpClient)
     }
 
     @Singleton
     @Provides
-    fun provideOkHttpClient() : OkHttpClient{
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder().build()
     }
 }
